7f2e2c5742ed20c1a95ba6f3e7f306a8f8bea60c
Enock <djsean@Enocks-MacBook-Pro.local>
2025-06-12 19:10:43 +0300

    new neon Db

--- yummytummy_project/settings.py
+++ yummytummy_project/settings.py
@@ -113,7 +113,7 @@ WSGI_APPLICATION = 'yummytummy_project.wsgi.application'
 DATABASE_URL = config('DATABASE_URL', default=None)
 
 if DATABASE_URL:
-    # Production database (PostgreSQL on Render)
+    # Production database (Neon PostgreSQL)
     DATABASES = {
         'default': dj_database_url.parse(DATABASE_URL)
     }
